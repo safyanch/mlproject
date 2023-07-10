f6977f9e761303b6d19094dab5ab10803f73fef0
safyanch <126394074+safyanch@users.noreply.github.com>
2023-07-10 07:42:17 +0500

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Dr. M. Safyan',
+packages=find_packages(),
+install_requires=['pandas', 'numpy', 'seaborn'],
+)
